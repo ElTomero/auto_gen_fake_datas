329bcdaec25d29668d65af56253ebbbc5db4ba10
ElTomero <thomas.siliotti@protonmail.com>
2023-04-12 11:44:37 +0200

    [EDIT]add if for generation

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ f = Faker()
 
 while True:
     
-    i=input('press 1 to continue gen datas\n press 2 for exit:\n') 
+    i=int(input('press 1 to continue gen datas\n press 2 for exit:\n')) 
     file_name=input('\nNome_file: ')    
     
-    if i == '2':
+    if i == 2:
         break
     
-    elif i == '1':
+    elif i == 1:
         
         table = input('inserisci nome tabella:')
-        column = input('inserisci attributo:')
-        times = input(input('quante volte:'))
-        
+        columns = input('Inserisci nomi delle colonne separate da virgole: ').split(',')
+        times = int(input('quante volte:'))
+      
         file = open(file_name, 'w')
         
         for _ in range(times):
-            data = f.name
-            file.write(f'\nINSERT INTO {table}({column}) \n VALUES')
+            data = str(f.name())
+            file.write(f'\nINSERT INTO {table}({", ".join(columns)}) \n VALUES({data});')
     
         file.close()
     else:
