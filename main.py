f73f22e18e8a981e745bae50eb8240f555133943
ElTomero <thomas.siliotti@protonmail.com>
2023-04-13 16:25:23 +0200

    [EDIT] code working for datas(int, char, date, float)

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 from faker import Faker
+import random
 
-f = Faker()
-
+fake = Faker()
 
 while True:
+    q = int(input('Press 1 to continue generating data\nPress 2 to exit: '))
     
-    i=int(input('press 1 to continue gen datas\n press 2 for exit:\n')) 
-    file_name=input('\nNome_file: ')    
-    
-    if i == 2:
+    if q == 2:
         break
-    
-    elif i == 1:
+    elif q == 1:
+        file_name = input('Nome file: ')  
+        table = input('Inserisci nome tabella:')
+        columns = input('Inserisci nomi delle colonne separate da virgole: ').split(', ')
+        times = int(input('Quante volte:'))
+        typeOfData = input('Inserisci i tipi di dato rispettivamente per ogni colonna separati da virgole (char, int, point, date): ').split(', ') 
         
-        table = input('inserisci nome tabella:')
-        columns = input('Inserisci nomi delle colonne separate da virgole: ').split(',')
-        times = int(input('quante volte:'))
-      
-        file = open(file_name, 'w')
-        
-        for _ in range(times):
-            data = str(f.name())
-            file.write(f'\nINSERT INTO {table}({", ".join(columns)}) \n VALUES({data});')
-    
-        file.close()
+        with open(file_name, 'w') as f:
+            for _ in range(times):
+                a = []
+                col_idx = 0
+                
+                for col_type in typeOfData:
+                    if col_type == 'char':
+                        a.append(f"'{fake.first_name_male()}'")
+                    elif col_type == 'int':
+                        a.append(str(random.randrange(10)))
+                    elif col_type == 'point':
+                        a.append(f"'{fake.coordinate()}'")
+                    elif col_type == 'date':
+                        a.append(f"'{fake.date()}'")
+                    
+                    col_idx += 1
+                
+                f.write(f"INSERT INTO {table} ({', '.join(columns)})\nVALUES ({', '.join(a)});\n")
     else:
         continue
